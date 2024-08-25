89bb7f8413b805e6e896d01bff40335d0a419062
Jack251970 <1160210343@qq.com>
2024-08-25 15:28:40 +0800

    improve auto select rich text box

--- src/Flow.Launcher.Plugin.ClipboardPlus.Panels/UserControls/AutoSelectRichTextBox.cs
+++ src/Flow.Launcher.Plugin.ClipboardPlus.Panels/UserControls/AutoSelectRichTextBox.cs
@@ -5,6 +5,44 @@ namespace Flow.Launcher.Plugin.ClipboardPlus.Panels.UserControls;
 
 public class AutoSelectRichTextBox : RichTextBox
 {
+    public static readonly DependencyProperty UnicodeTextProperty = DependencyProperty.Register(
+        nameof(UnicodeText),
+        typeof(string),
+        typeof(AutoSelectRichTextBox),
+        new PropertyMetadata(default(string), OnUnicodeTextPropertyChanged)
+    );
+
+    private static void OnUnicodeTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        RichTextBox rtb = (RichTextBox)d;
+        rtb.SetUnicodeText((string)e.NewValue);
+    }
+
+    public string UnicodeText
+    {
+        get => (string)GetValue(UnicodeTextProperty);
+        set { SetValue(UnicodeTextProperty, value); }
+    }
+
+    public static readonly DependencyProperty RichTextProperty = DependencyProperty.Register(
+        nameof(RichText),
+        typeof(string),
+        typeof(AutoSelectRichTextBox),
+        new PropertyMetadata(default(string), OnRichTextPropertyChanged)
+    );
+
+    private static void OnRichTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        RichTextBox rtb = (RichTextBox)d;
+        rtb.SetRichText((string)e.NewValue);
+    }
+
+    public string RichText
+    {
+        get => (string)GetValue(RichTextProperty);
+        set { SetValue(RichTextProperty, value); }
+    }
+
     public AutoSelectRichTextBox()
     {
         Focusable = true;
