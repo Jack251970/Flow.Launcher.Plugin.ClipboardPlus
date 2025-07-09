caa4bfd5e92c2b28ea2eb2f6f3a27b61c0025846
Jack251970 <1160210343@qq.com>
2025-07-09 20:04:52 +0800

    Fix resource initialization issue

--- src/Flow.Launcher.Plugin.ClipboardPlus.Panels.Test/App.xaml.cs
+++ src/Flow.Launcher.Plugin.ClipboardPlus.Panels.Test/App.xaml.cs
@@ -8,8 +8,8 @@ public partial class App : Application
 {
     public App()
     {
-        AddResources();
         InitializeComponent();
+        AddResources();
     }
 
     private void AddResources()
